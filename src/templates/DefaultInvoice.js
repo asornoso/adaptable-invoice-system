3cdfc3fd67fd9285447ae993f72bb1c6f38953e7
Adam Sornoso <asornoso@yahoo.com>
2020-09-30 11:47:01 -0700

    style updates to default_invoice

--- src/templates/DefaultInvoice.js
+++ src/templates/DefaultInvoice.js
@@ -42,50 +42,55 @@ const DefaultInvoice = {
 
       console.log(props)
       if(props === undefined || Object.keys(props).length === 0 )
-        return (<div>No props passed to layout</div>)
+        return (<div>No record passed to layout</div>)
 
       const images = require.context('../resources', true)
       const logo = images(`./${configuration.companyInfo.brand.logo}`)
+      console.log(props.purchase.items)
+
+      const rows =   props.purchase.items.map((item, i) => {
+          return <tr key={'row_'+i} className={i % 2 === 0 ? 'light-row' : 'dark-row'}><td>{item.name}</td><td>{item.price}</td></tr>
+        })
 
       return (
-        <div className='invoice-wrapper'>
-          <div className='company-info'>
-            <h2>{configuration.companyInfo.brand.name}</h2>
-            <h4>{configuration.companyInfo.contact.streetAddress}</h4>
-            <h4>{configuration.companyInfo.contact.stateAddress}</h4>
-            <h4>{configuration.companyInfo.contact.phone}</h4>
-            <h4>{configuration.companyInfo.contact.fax}</h4>
-            <h4>{configuration.companyInfo.contact.website}</h4>
-          </div>
-          <div className='company-logo'>
-              <img className='logo' src={logo} alt='logo'/>
-          </div>
-          <div className='customer-info'>
-            <p>Customer Name: {props.customer.name}</p>
-            <p>Customer ID: {props.customer.id}</p>
-          </div>
-          <div className='purchase-info'>
-            <p> Invoice ID: {props.id} </p>
-            <p>Transaction ID: {props.purchase.id}</p>
-            <p>Date: {props.purchase.date}</p>
-            <p>Total: {props.purchase.amount}</p>
-          </div>
-          <div className='table'>
-            <table>
-              <thead>
-                <tr><th>Item Name:</th><th>Item Price:</th></tr>
-              </thead>
-              <tbody>
-              {
-                props.purchase.items.forEach((item, i) => {
-                  return <tr><td>{item.name}</td><td>{item.price}</td></tr>
-                })
-              }
-              </tbody>
-            </table>
+        <div className="center-wrapper">
+          <div className='invoice-wrapper'>
+            <div className='company-info'>
+              <h2>{configuration.companyInfo.brand.name}</h2>
+              <h4>{configuration.companyInfo.contact.streetAddress}</h4>
+              <h4>{configuration.companyInfo.contact.stateAddress}</h4>
+              <h4>{configuration.companyInfo.contact.phone}</h4>
+              <h4>{configuration.companyInfo.contact.fax}</h4>
+              <h4>{configuration.companyInfo.contact.website}</h4>
+            </div>
+            <div className='company-logo'>
+                <img className='logo' src={logo} alt='logo'/>
+            </div>
+            <div className='customer-info'>
+              <p>Customer Name: {props.customer.name}</p>
+              <p>Customer ID: {props.customer.id}</p>
+            </div>
+            <div className='purchase-info'>
+              <p> Invoice ID: {props.id} </p>
+              <p>Transaction ID: {props.purchase.id}</p>
+              <p>Date: {props.purchase.date}</p>
+              <p>Total: {props.purchase.amount}</p>
+            </div>
+            <div className='table'>
+              <table>
+                <thead>
+                  <tr className='header-row'><th>Item Name:</th><th>Item Price:</th></tr>
+                </thead>
+                <tbody>
+                {
+                  rows
+                }
+                </tbody>
+              </table>
+
+            </div>
 
           </div>
-
         </div>
       )
     }
